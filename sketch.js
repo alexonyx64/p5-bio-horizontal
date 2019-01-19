a7004f5698c2349fdca54557880f804d969ae142
alexonyx64 <45368448+alexonyx64@users.noreply.github.com>
2019-01-19 14:26:35 -0500

    removed headshot

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-var headshot;
-function preload() {
-	headshot = loadImage('headshot.png');
-}
 function setup() {
 	createCanvas(700, 400);
 }
@@ -10,5 +6,4 @@ function draw() {
 	noStroke();
 	fill(140,255,200);
 	rect(70,0,700,400);
-	image(headshot,0,100);
 }
